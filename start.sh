b1309a4b1892d4f8772b4871a337181d0b2e1afb
Rafa <rafael.nieto@viewnext.com>
2024-12-24 11:06:52 +0100

    desactivados logs de warning

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 # Crear el entorno virtual
 python3 -m venv venv
 
-# Activarlo
+# Activar entorno virtual: Mac
 source venv/bin/activate
+
+# Activar entorno virtual: Windows
+venv\Scripts\activate
 
 # Instalar dependencias
 pip install -r requirements.txt
@@ -10,10 +13,13 @@ python -m spacy download es_core_news_sm
 
 # ----------------------------------------------------------------
 
-# Ejecutar preprocesamiento
+# 1. Primero preprocesar los documentos
 python src/app.py --mode preprocess
 
-# Ejecutar entrenamiento
+# 2. Luego analizar con Ollama
+python src/app.py --mode analyze
+
+# 3. Finalmente entrenar
 python src/app.py --mode train
 
 # Ejecutar pipeline completa
